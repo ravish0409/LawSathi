3ea05ea690c51a6c7956fb2897466f00febbd010
Rav <109892241+ravish0409@users.noreply.github.com>
2024-06-23 17:01:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "openAI............key"; // Paste your API key here
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
